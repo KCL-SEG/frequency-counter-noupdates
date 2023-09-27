19a4a06061b10085cbb9fa3e700886d84abfa1d3
that guy <user@DESKTOP-EOQPDVD.localdomain>
2023-09-27 16:35:15 +0100

    exercise 2

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,11 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+items = ["a", "a", "b", "b", "b", "c"]
+
 def frequencies(items):
-    frequencies = {}
+    frequencies = {i:items.count(i) for i in items}
     # Your code goes here
     return frequencies
+
+print(frequencies(items))
