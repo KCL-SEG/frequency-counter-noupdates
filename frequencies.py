a7dbb6882e811a8a42afd4f4a0befdf76e314c0d
that guy <user@DESKTOP-EOQPDVD.localdomain>
2023-10-01 01:11:41 +0100

    fixed commas

--- frequencies.py
+++ frequencies.py
@@ -1,10 +1,22 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
-items = ["a", "a", "b", "b", "b", "c"]
+items = [100, 'Hello', '100', '100', 100]
 
-def frequencies(items):
-    frequencies = {i:items.count(i) for i in items}
+
+
+
+
+
+def frequencies(items):   
+    itemsTwo = []
+
+    for x in items:
+        x = str(x)
+        itemsTwo.append(x)
+
+    #frequencies = {i:items.count(i) for i in items}
+    frequencies = {i:itemsTwo.count(i) for i in itemsTwo}
     # Your code goes here
     return frequencies
 
